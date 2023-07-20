34c1b5cb05adc88c6f7e74825505b7088a3155f9
Tony Gardella <AGardella@indecon.com>
2023-07-20 14:11:04 -0400

    add data folder

--- createSystemData/update_data.R
+++ createSystemData/update_data.R
@@ -5,15 +5,19 @@ library(piggyback)
 
 projectPath      <- "./createSystemData/"
 dataOutPath      <- projectPath %>% file.path("data")
-
+print("Hello")
 ## Get latest updated data
 latest_release_fredi_Data <- piggyback::pb_releases(repo = "USEPA/FrEDI_data") |>
                              filter(published_at == max(published_at))
+
+print("Hello1")
 
 ## Grab data from latest release
 pb_download(repo = "USEPA/FrEDI_data",
             tag = latest_release_fredi_Data$release_name,
             dest = tempdir())
+
+print("Hello2")
 
 file.copy(
   from = file.path(tempdir(),"sysdata.rda"),
